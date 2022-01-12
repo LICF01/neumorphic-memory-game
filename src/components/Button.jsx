4bddd1d5da99e2df9066157232a85b4319cc90bc
LICF01 <lucascubilla@gmail.com>
2022-01-12 04:10:54 -0300

    Added click handler and disabled check

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from 'react';
 import './Button.css';
 
-const Button = ({ button, selected }) => {
+const Button = ({ button, selected, handleSelect, disabled }) => {
+	const handleClick = () => {
+		if (!disabled) {
+			handleSelect(button);
+		}
+	};
+
 	return (
 		<div>
-			<button className={selected ? 'button selected' : 'button unselected'}>
+			<button
+				className={selected ? 'button selected' : 'button unselected'}
+				onClick={handleClick}
+			>
 				<i className={button.name} style={{ color: button.color }}></i>
 			</button>
 		</div>
